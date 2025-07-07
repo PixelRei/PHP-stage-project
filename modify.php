151e912fca0d311c7f7d840a2b9e53fbe377a1c7
PixelRei <playboi.rei17@gmail.com>
2025-07-07 15:54:41 +0200

    Add files via upload

--- modify.php
+++ modify.php
@@ -17,7 +17,6 @@ $id = $_SESSION['id'];
     <link rel="icon" href="img/moon-icon.png">
 </head>
 <body>
-    <div class="login-container ">
         <div class="panel">
             <div class="info">
                 <p class="credentials">Username: <?=$username?></p>
@@ -37,103 +36,107 @@ $id = $_SESSION['id'];
                 <a id="created" href="index.php"><input id="button" type="submit" name="submit" value="Modifica"></a>
                 </form>
             </div>
-            <?php   
-                if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){ //condition to see if the form was sent correctly
-                    
-                //update the session
-                $new_username = $_POST['username'];
-                $new_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                $_SESSION['username'] = $new_username;
-                $_SESSION['password'] = $_POST['password'];
-                //and also the database
-                try {   
-                    $hostname = '127.0.0.1';
-                    $dbname = 'mydatabse';
-                    $user = 'root';
-                    $pass = '';
-                    $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass);
-                } catch (PDOException $e) {
-                    echo "error: ".$e->getMessage();
-                    die();
-                }
-                $sql = "UPDATE people SET username = :username, password = :password WHERE id = :id";
-                $stmt = $db->prepare($sql);
-                $stmt->bindValue(':username', $new_username, PDO::PARAM_STR);
-                $stmt->bindValue(':password', $new_password, PDO::PARAM_STR);
-                $stmt->bindValue(':id', $id, PDO::PARAM_STR);
-                $stmt->execute();
-                header("Location: index.php");
-                exit;
-                }
-            ?>
-            <!---script for image management--->
-            <?php
-            $uploadOk = 0;
-            $targetFile = "";
-
-            if (isset($_POST['submit-image'])) {
-                if (isset($_FILES["image"])) {
-                    $directory = 'uploads/';
-                    $targetFile = $directory . basename($_FILES["image"]["name"]);
-                    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-                    // Controlla se è un'immagine
-                    $check = getimagesize($_FILES["image"]["tmp_name"]);
-                    if ($check !== false) {
-                        echo "Il file è un'immagine - " . $check["mime"] . ".<br>";
-                        $uploadOk = 1;
-                    } else {
-                        echo "Il file non è un'immagine.<br>";
-                        $uploadOk = 0;
+            <div class="image-uploader">
+                <?php   
+                    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])){ //condition to see if the form was sent correctly
+                        
+                    //update the session
+                    $new_username = $_POST['username'];
+                    $new_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                    $_SESSION['username'] = $new_username;
+                    $_SESSION['password'] = $_POST['password'];
+                    //and also the database
+                    try {   
+                        $hostname = '127.0.0.1';
+                        $dbname = 'mydatabse';
+                        $user = 'root';
+                        $pass = '';
+                        $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+                    } catch (PDOException $e) {
+                        echo "error: ".$e->getMessage();
+                        die();
                     }
-
-                    /* Controlla se il file esiste già
-                    if (file_exists($targetFile)) {
-                        echo "Il file esiste già.<br>";
-                        $uploadOk = 0;
-                    }*/
-
-                    // Controlla la dimensione del file
-                    if ($_FILES["image"]["size"] > 500000) {
-                        echo "Il file è troppo grande.<br>";
-                        $uploadOk = 0;
+                    $sql = "UPDATE people SET username = :username, password = :password WHERE id = :id";
+                    $stmt = $db->prepare($sql);
+                    $stmt->bindValue(':username', $new_username, PDO::PARAM_STR);
+                    $stmt->bindValue(':password', $new_password, PDO::PARAM_STR);
+                    $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+                    $stmt->execute();
+                    header("Location: index.php");
+                    exit;
                     }
+                ?>
+                <!---script for image management--->
+                <?php
+                $uploadOk = 0;
+                $targetFile = "";
 
-                    // Controlla il formato del file
-                    if (!in_array($imageFileType, ["jpg", "jpeg", "png", "gif"])) {
-                        echo "Solamente i file JPG, JPEG, PNG & GIF sono accettati.<br>";
-                        $uploadOk = 0;
-                    }
+                if (isset($_POST['submit-image'])) {
+                    if (isset($_FILES["image"])) {
+                        $directory = 'uploads/';
+                        $targetFile = $directory . basename($_FILES["image"]["name"]);
+                        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-                    // Prova a caricare il file
-                    if ($uploadOk == 0) {
-                        echo "Il file non è stato caricato correttamente.<br>";
-                    } else {
-                        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-                            echo "Il file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " è stato caricato con successo.<br>";
+                        // Controlla se è un'immagine
+                        $check = getimagesize($_FILES["image"]["tmp_name"]);
+                        if ($check !== false) {
+                            //echo "Il file è un'immagine - " . $check["mime"] . ".<br>";
+                            $uploadOk = 1;
                         } else {
-                            echo "C'è stato un errore durante il caricamento.<br>";
+                            echo "Il file non è un'immagine.<br>";
+                            $uploadOk = 0;
+                        }
+
+                        /* Controlla se il file esiste già
+                        if (file_exists($targetFile)) {
+                            echo "Il file esiste già.<br>";
+                            $uploadOk = 0;
+                        }*/
+
+                        // Controlla la dimensione del file
+                        if ($_FILES["image"]["size"] > 500000) {
+                            echo "Il file è troppo grande.<br>";
+                            $uploadOk = 0;
+                        }
+
+                        /* Controlla il formato del file
+                        if (!in_array($imageFileType, ["jpg", "jpeg", "png", "gif"])) {
+                            echo "Solamente i file JPG, JPEG, PNG & GIF sono accettati.<br>";
+                            $uploadOk = 0;
+                        }*/
+
+                        // Prova a caricare il file
+                        if ($uploadOk == 0) {
+                            echo "Il file non è stato caricato correttamente.<br>";
+                        } else {
+                            if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+                                echo "Il file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " è stato caricato con successo.<br>";
+                            } else {
+                                echo "C'è stato un errore durante il caricamento.<br>";
+                            }
                         }
                     }
                 }
-            }
-            try {
-                    $hostname = '127.0.0.1';
-                    $dbname = 'mydatabse';
-                    $user = 'root';
-                    $pass = '';
-                    $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass);
-                } catch (PDOException $e) {
-                    echo "error: ".$e->getMessage();
-                    die();
-                }
-            $sql = "UPDATE people SET image = :targetFile WHERE id = :id";
-            $stmt= $db->prepare($sql);
-            $stmt->bindValue(':targetFile', $targetFile, PDO::PARAM_STR);
-            $stmt->bindValue(':id', $_SESSION['id'], PDO::PARAM_INT);
-            $stmt->execute();
-            ?>
+                try {
+                        $hostname = '127.0.0.1';
+                        $dbname = 'mydatabse';
+                        $user = 'root';
+                        $pass = '';
+                        $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+                    } catch (PDOException $e) {
+                        echo "error: ".$e->getMessage();
+                        die();
+                    }
+                $sql = "UPDATE people SET image = :targetFile WHERE id = :id";
+                $stmt= $db->prepare($sql);
+                $stmt->bindValue(':targetFile', $targetFile, PDO::PARAM_STR);
+                $stmt->bindValue(':id', $_SESSION['id'], PDO::PARAM_INT);
+                $stmt->execute();
+                ?>
+                <?php if (isset($_FILES["image"]) && $uploadOk == 1) : ?>
+                    <img src="<?php echo $targetFile; ?>"width="80%" alt="Uploaded Image">
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
 </body>
 </html>
