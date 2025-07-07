c0a981d6ab5a398d460ed9fefb507e498f60bf3c
PixelRei <playboi.rei17@gmail.com>
2025-07-07 11:05:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const modify = document.querySelector(".modify_button");
-    modify.addEventListener("click", ()=>{
-        alert("credenziali modificate con successo");
-    });
-});
