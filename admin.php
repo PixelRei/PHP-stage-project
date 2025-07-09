03b2d65b7c5112c3db858f29c47be27653a6114e
PixelRei <playboi.rei17@gmail.com>
2025-07-09 08:40:30 +0200

    Add files via upload

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Admin Panel - Modify Users</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="img/moon-icon.png">
 </head>
